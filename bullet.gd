76f4ef90c6c403d08961672c4631620583045871
jerrysct <jerry.br.huang@gmail.com>
2026-05-11 17:04:13 +0800

    重傳劉哲愷的檔案

---
+++ bullet.gd
@@ -0,0 +1,18 @@
+# Bullet.gd
+extends Area2D
+
+var direction = Vector2.RIGHT
+var speed = 200.0
+var damage = 10.0
+var is_reflected = false
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+# 被反彈時呼叫此函式
+func reflect(new_direction: Vector2, multiplier: float = 1.0):
+	direction = new_direction
+	damage *= multiplier
+	is_reflected = true
+	# 改變顏色或特效來區分
+	modulate = Color.GOLD
